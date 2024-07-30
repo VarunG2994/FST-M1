eb42dd460dd0a09293c655930f1de88289520d8a
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-07-30 19:08:49 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,3 @@
+public class Activity8 {
+    //This is to run all the test parallely.
+}
---
+++ TestNG/Activities/DemoTwo.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+    @Test
+    public void TestCase() {
+
+    }
+}
