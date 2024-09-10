70e55db2541afba666643538536f20a7561b0e77
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-09-10 14:59:35 +0530

    Update Activity1.java

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -1,48 +1,60 @@
+package Examples;
+
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Activity1 {
-
+public class Activity1
+{
+    // Driver Declaration
     AndroidDriver driver;
 
+    // Setting up
     @BeforeClass
-
     public void setUp() throws MalformedURLException {
-        UiAutomator2Options options= new UiAutomator2Options();
+
+
+        UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("android");
         options.setAutomationName("UiAutomator2");
         options.setAppPackage("com.google.android.calculator");
         options.setAppActivity("com.android.calculator2.Calculator");
+        options.noReset();
 
+        // Server Address
         URL serverURL = new URL("http://localhost:4723/");
+
+        // Driver Initialization
         driver = new AndroidDriver(serverURL, options);
     }
 
+    // Test method
     @Test
-
-    public void multiply(){
-        driver.findElement(AppiumBy.id("digit_6")).click();
-        driver .findElement(AppiumBy.accessibilityId("multiply")).click();
-        driver.findElement(AppiumBy.id("digit_5")).click();
-        driver.findElement(AppiumBy.accessibilityId("equals")).click();
-
-        String result= driver.findElement(AppiumBy.id("result_final")).getText();
-        System.out.println("Result is "+ result);
+    public void multiplyTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_5")).click();
+        driver.findElement(AppiumBy.id("com.google.android.calculator:id/op_mul")).click();
+        driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_4")).click();
+        driver.findElement(AppiumBy.id("com.google.android.calculator:id/eq")).click();
 
 
+        String product = driver.findElement(AppiumBy.id("result_final")).getText();
+
+        // Assertion
+        Assert.assertEquals(product, "20");
     }
 
 
+    // Tear down method
     @AfterClass
-    public void teaDown(){
+    public void tearDown() {
+        // Close the app
         driver.quit();
     }
-
 }
