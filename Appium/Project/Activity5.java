2e91c4b732587e30bb82c4c3f8cee64db328b96f
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-09-10 15:00:43 +0530

    Update Activity5.java

--- Appium/Project/Activity5.java
+++ Appium/Project/Activity5.java
@@ -1,14 +1,22 @@
+package Appium_Project;
+
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+
 
 public class Activity5
 {
