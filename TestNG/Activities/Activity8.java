ea75b8baa0e1e5a820003c6c03a8a056a5a45832
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-09-10 14:35:20 +0530

    Update Activity8.java

--- TestNG/Activities/Activity8.java
+++ TestNG/Activities/Activity8.java
@@ -3,3 +3,20 @@ package suiteExample;
 public class Activity8 {
     //This is to run all the test parallely.
 }
+
+/*
+<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd" >
+<suite name="TestSuite" parallel="classes">
+    <test name="Tests">
+        <classes>
+            <class name="Activity1"/>
+            <class name="Activity2"/>
+            <class name="suiteExample.DemoOne"/>
+            <class name="suiteExample.DemoTwo"/>
+            <class name="Activity5"/>
+            <class name="Activity6"/>
+        </classes>
+    </test>
+</suite>
+
+*/
