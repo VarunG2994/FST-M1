a8a34a1e0d705618c41580879ead2a5500abbb2f
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-09-12 14:19:06 +0530

    Update Activity5.java

--- Appium/Activities/Activity5.java
+++ Appium/Activities/Activity5.java
@@ -1,8 +1,7 @@
 
+package activities;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,10 +9,12 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class Activity5 {
     // Driver Declaration
