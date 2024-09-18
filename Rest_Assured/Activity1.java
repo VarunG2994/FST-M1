aaecfa83e6bee66a5a053ff38d1c89ca44b2e944
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-09-18 20:05:54 +0530

    Update Activity1.java

--- Rest_Assured/Activity1.java
+++ Rest_Assured/Activity1.java
@@ -1,59 +1,61 @@
 package activities;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-import org.testng.annotations.Test;
-
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-
 public class Activity1 {
+
     // Set base URL
     final static String ROOT_URI = "https://petstore.swagger.io/v2/pet";
 
     @Test(priority=1)
     public void addNewPet() {
         // Create JSON request
-        String reqBody = "{"
-                + "\"id\": 77232,"
-                + "\"name\": \"Riley\","
-                + " \"status\": \"alive\""
-                + "}";
+        Map<String, Object> reqBody = new HashMap<>();
+        reqBody.put("id", 2039);
+        reqBody.put("name", "Shifu");
+        reqBody.put("status", "alive");
 
         Response response =
-                given().contentType(ContentType.JSON) // Set headers
-                        .body(reqBody) // Add request body
-                        .when().post(ROOT_URI); // Send POST request
+                given().contentType(ContentType.JSON)
+                        .body(reqBody)
+                        .when().post(ROOT_URI);
 
         // Assertion
-        response.then().body("id", equalTo(77232));
-        response.then().body("name", equalTo("Riley"));
+        response.then().body("id", equalTo(2039));
+        response.then().body("name", equalTo("Shifu"));
         response.then().body("status", equalTo("alive"));
     }
 
     @Test(priority=2)
     public void getPetInfo() {
         Response response =
-                given().contentType(ContentType.JSON) // Set headers
-                        .when().pathParam("petId", "77232") // Set path parameter
-                        .get(ROOT_URI + "/{petId}"); // Send GET request
+                given().contentType(ContentType.JSON)
+                        .when().pathParam("petId", "2039")
+                        .get(ROOT_URI + "/{petId}");
 
         // Assertion
-        response.then().body("id", equalTo(77232));
-        response.then().body("name", equalTo("Riley"));
+        response.then().body("id", equalTo(2039));
+        response.then().body("name", equalTo("Shifu"));
         response.then().body("status", equalTo("alive"));
     }
 
     @Test(priority=3)
     public void deletePet() {
         Response response =
-                given().contentType(ContentType.JSON) // Set headers
-                        .when().pathParam("petId", "77232") // Set path parameter
-                        .delete(ROOT_URI + "/{petId}"); // Send DELETE request
+                given().contentType(ContentType.JSON)
+                        .when().pathParam("petId", 2039)
+                        .delete(ROOT_URI + "/{petId}");
 
         // Assertion
         response.then().body("code", equalTo(200));
-        response.then().body("message", equalTo("77232"));
+        response.then().body("message", equalTo("2039"));
     }
 }
