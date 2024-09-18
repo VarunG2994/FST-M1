61de357128fe02e95011cf2a7754fd04d73d0896
VarunG2994 <156886671+VarunG2994@users.noreply.github.com>
2024-09-18 20:14:42 +0530

    Update Activity5.java

--- Appium/Project/Activity5.java
+++ Appium/Project/Activity5.java
@@ -1,62 +1,61 @@
-package Appium_Project;
+package project;
 
+import activities.ActionsBase;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.touch.offset.PointOption;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+public class Activity5 {
 
-public class Activity5
-{
     AndroidDriver driver;
     WebDriverWait wait;
 
-    // Set up method
     @BeforeClass
-    public void setUp() throws MalformedURLException {
-        // Desired Capabilities
+    public void SetUp() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setPlatformName("android");
+        options.setPlatformName("Android");
         options.setAutomationName("UiAutomator2");
         options.setAppPackage("com.android.chrome");
         options.setAppActivity("com.google.android.apps.chrome.Main");
         options.noReset();
 
-        // Server Address
-        URL serverURL = new URL("http://localhost:4723/");
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
 
-        // Driver Initialization
         driver = new AndroidDriver(serverURL, options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        // Open the page in Chrome
         driver.get("https://v1.training-support.net/selenium");
     }
+
     @Test
-    public void login() throws InterruptedException
-    {
+    public void loginTestValid() throws InterruptedException {
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@text=\"Get Started!\"]"))).click();
+
         String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
-        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.view.View")));
-        driver.findElement(AppiumBy.androidUIAutomator(UiScrollable + ".flingForward()"));
+        driver.findElement(AppiumBy.androidUIAutomator(UiScrollable + ".scrollTextIntoView(\"Login Form\")"));
 
-        Thread.sleep(1000);
-        //driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"To-Do List\"]")).click();
-        driver.findElement(AppiumBy.xpath("")).click();
 
-        //Login Form  Please sign in.
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"Login Form\"]"))).click();
+        Thread.sleep(5000);
+        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Login Form\"]/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]")).sendKeys("admin");
+        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Login Form\"]/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]")).sendKeys("password");
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Log in\"]")).click();
 
+        Assert.assertTrue(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Welcome Back, admin\"]")).isDisplayed());
     }
 
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 }
